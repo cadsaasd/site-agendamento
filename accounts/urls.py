ab3bd386fa059ed96799fa59511ea9fa1c8629a9
caua <cauasilva3643@gmail.com>
2026-07-06 16:09:46 -0300

    Remove arquivos CSS antigos e atualiza estrutura de templates para melhorar o layout e a responsividade das páginas de login e cadastro.

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', views.login_view, name = 'login'),
-    path('registrar/', views.register_view, name = 'register'),
+    path('login/', views.login_view, name = 'login'),
+    path('cadastro/', views.register_view, name = 'register'),
 ]
