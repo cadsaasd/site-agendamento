95c12dddf352ebbd5c477fe1e21f02bc2cfcb0fd
caua <cauasilva3643@gmail.com>
2026-07-06 16:30:15 -0300

    Remove estilo da opção 'Lembrar de mim', corrige título da página de login e adiciona nova página para verificação de código do WhatsApp.

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.decorators import login_required
 
+
 def login_view(request):
     return render(request, 'accounts/pages/login.html')
 
