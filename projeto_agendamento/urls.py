164d0c504b49546f318e4821174eb69eac6441ff
caua <cauasilva3643@gmail.com>
2026-08-28 15:49:48 -0300

    Adiciona app 'main' com configuração, URLs, views, templates e testes

--- projeto_agendamento/urls.py
+++ projeto_agendamento/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include  # type: ignore[import]
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('accounts.urls')),
+    path('accounts/', include('accounts.urls')),
+    path('', include('main.urls'))
 ]
