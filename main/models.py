719e8bd8ab7dfca4fedc88fab8e7206ae351f1be
caua <cauasilva3643@gmail.com>
2026-08-30 20:21:59 -0300

    feat(main): adiciona modelos e views para a pagina home

--- main/models.py
+++ main/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Services(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    duration = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
--- main/views.py
+++ main/views.py
@@ -2,4 +2,29 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/pages/home.html')
+    services = [
+        {
+            "nome": "Manicure",
+            "descricao": "Cuidado completo das unhas",
+            "duracao": 40,
+            "preco": 30,
+        },
+        {
+            "nome": "Pedicure",
+            "descricao": "Cuidados e esmaltação dos pés",
+            "duracao": 50,
+            "preco": 40,
+        },
+        {
+            "nome": "Alongamento",
+            "descricao": "Alongamento personalizado das unhas",
+            "duracao": 90,
+            "preco": 100,
+        },
+        
+               
+    ]
+
+    return render(request, 'main/pages/home.html',{
+        "services": services
+    })
