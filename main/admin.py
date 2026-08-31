f8e6d9dfe3c6560c4a3f8cebe77995c97ba7c8ac
caua <cauasilva3643@gmail.com>
2026-08-31 17:00:00 -0300

    feat(main): gera migracoes iniciais e configura django admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Services
 # Register your models here.
+
+class ServicesAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Services, ServicesAdmin)
