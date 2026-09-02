97888b9d31ac7c28e603d40b76a7545c12dc49ba
caua <cauasilva3643@gmail.com>
2026-09-02 13:31:30 -0300

    refactor(services): atualiza estrutura e corrige nomenclaturas nos templates

--- main/views.py
+++ main/views.py
@@ -1,30 +1,9 @@
 from django.shortcuts import render
+from .models import Services
 
 # Create your views here.
 def home(request):
-    services = [
-        {
-            "nome": "Manicure",
-            "descricao": "Cuidado completo das unhas",
-            "duracao": 40,
-            "preco": 30,
-        },
-        {
-            "nome": "Pedicure",
-            "descricao": "Cuidados e esmaltação dos pés",
-            "duracao": 50,
-            "preco": 40,
-        },
-        {
-            "nome": "Alongamento",
-            "descricao": "Alongamento personalizado das unhas",
-            "duracao": 90,
-            "preco": 100,
-        },
-        
-               
-    ]
-
+    services = Services.objects.all()
     return render(request, 'main/pages/home.html',{
         "services": services
     })
