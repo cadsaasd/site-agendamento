ca05bdcd3d5705508c38e6feb70f107a0e872aaa
caua <cauasilva3643@gmail.com>
2026-09-03 15:09:27 -0300

    feat(services): adiciona filtragem por categoria com JavaScript

---
+++ main/static/main/js/services.js
@@ -0,0 +1,20 @@
+const tabs = document.querySelectorAll('.tab-btn');
+
+tabs.forEach(tab => 
+    tab.addEventListener('click', () => tabClicked(tab))
+);
+
+const tabClicked = (tab) => {
+
+    const contents = document.querySelectorAll('.content');
+    const tabs = document.querySelectorAll('.tab-btn');
+
+    contents.forEach(content => content.classList.remove('show'));
+    tabs.forEach(tab => tab.classList.remove('active'));
+
+    const contentId = tab.getAttribute('content-id');
+    const content = document.getElementById(contentId);
+
+    content.classList.add('show');
+    tab.classList.add('active');
+}
