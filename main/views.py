7519d162bcbe2ed6c2a4e6838f11032d89d377ce
caua <cauasilva3643@gmail.com>
2026-09-03 21:14:14 -0300

    feat(home): organiza imagens do carrossel por categoria

--- main/views.py
+++ main/views.py
@@ -1,21 +1,36 @@
 from django.shortcuts import render
+
 from .models import Services, LandingImages
 
-# Create your views here.
+
 def home(request):
+
     services = Services.objects.all()
-    carousel_images = LandingImages.objects.filter(
+
+    nails_images = LandingImages.objects.filter(
         section="carousel",
-        is_active=True
-    ).order_by("order")[:6]
+        is_active=True,
+        category__name="nails"
+    ).order_by("order")
+
+    lash_brow_images = LandingImages.objects.filter(
+        section="carousel",
+        is_active=True,
+        category__name="lash_brow"
+    ).order_by("order")
+
+    carousel_images = []
+
+    for nail, lash in zip(nails_images, lash_brow_images):
+        carousel_images.append(nail)
+        carousel_images.append(lash)
 
     hero_image = LandingImages.objects.filter(
         section="hero",
         is_active=True
     ).order_by("order").first()
 
-
-    return render(request, 'main/pages/home.html',{
+    return render(request, 'main/pages/home.html', {
         "services": services,
         "carousel_images": carousel_images,
         "hero_image": hero_image
