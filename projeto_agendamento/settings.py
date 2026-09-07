9353f86c40ba4be71daff1785fb784bfe1eadf47
caua <cauasilva3643@gmail.com>
2026-09-07 19:30:46 -0300

    feat(env): adiciona variáveis de ambiente para configuração do projeto

--- projeto_agendamento/settings.py
+++ projeto_agendamento/settings.py
@@ -25,13 +25,12 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xzp$-(4@wiq!&)s#l21d(*mbyr0xg=k*3b94b23nxmg185v08p'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
 
 # Application definition
 
